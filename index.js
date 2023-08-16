e47f9edb54845c64a6815c1909fa4ecb50f4dc87
Katherine Greenwitch <131037184+KGreenwitch@users.noreply.github.com>
2023-08-15 19:46:39 -0700

    Add player and computer selection variables

--- index.js
+++ index.js
@@ -8,25 +8,29 @@ const getComputerChoice = () => {
         return "SCISSORS";
     }
 }
-const playRound = (playerSelection.toUpperCase(), computerSelection) => {
+let playerSelection = prompt().toUpperCase();
+let computerSelection = getComputerChoice();
+
+const playRound = (playerSelection, computerSelection) => {
     if (playerSelection === computerSelection) {
-        return "It is a tie!"
+        return "It is a tie!";
     } else if (playerSelection === "ROCK") {
         if (computerSelection === "SCISSORS") {
-            return "You win! Rock beats Scissors."
+            return "You win! Rock beats Scissors.";
         } else {
             return "You lose! Paper beats Rock."}
     } else if (playerSelection === "SCISSORS") {
         if (computerSelection === "PAPER") {
-            return "You win! Scissors beats Paper."
+            return "You win! Scissors beats Paper.";
         } else {
-            return "You lose! Rock beats Scissors."
+            return "You lose! Rock beats Scissors.";
         }
     } else if (playerSelection === "PAPER") {
         if (computerSelection === "ROCK") {
-            return "You win! Paper beats Rock."
+            return "You win! Paper beats Rock.";
         } else {
-            return "You lose! Scissors beats Paper."
+            return "You lose! Scissors beats Paper.";
         }
     }
 }
+console.log(playRound(playerSelection, computerSelection))
