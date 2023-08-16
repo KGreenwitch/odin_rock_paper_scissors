cd08b8f78c5b8a505f0fe2b49349104dde0006d1
Katherine Greenwitch <131037184+KGreenwitch@users.noreply.github.com>
2023-08-15 19:30:08 -0700

    Add function to play a round of the game

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 const getComputerChoice = () => {
     let choice = Math.floor(Math.random() * 3);
     if (choice === 0) {
-        return "Rock";
+        return "ROCK";
     } else if (choice === 1) {
-        return "Paper";
+        return "PAPER";
     } else if (choice === 2) {
-        return "Scissors";
+        return "SCISSORS";
+    }
+}
+const playRound = (playerSelection.toUpperCase(), computerSelection) => {
+    if (playerSelection === computerSelection) {
+        return "It is a tie!"
+    } else if (playerSelection === "ROCK") {
+        if (computerSelection === "SCISSORS") {
+            return "You win! Rock beats Scissors."
+        } else {
+            return "You lose! Paper beats Rock."}
+    } else if (playerSelection === "SCISSORS") {
+        if (computerSelection === "PAPER") {
+            return "You win! Scissors beats Paper."
+        } else {
+            return "You lose! Rock beats Scissors."
+        }
+    } else if (playerSelection === "PAPER") {
+        if (computerSelection === "ROCK") {
+            return "You win! Paper beats Rock."
+        } else {
+            return "You lose! Scissors beats Paper."
+        }
     }
 }
