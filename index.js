fe166954da0024b15ce595a4ee52d75329dcf1a7
Katherine Greenwitch <131037184+KGreenwitch@users.noreply.github.com>
2023-08-19 16:52:16 -0700

    Add win and lose count
    Created variables to track win and lose count. Used them in the play
    round function.

--- index.js
+++ index.js
@@ -8,25 +8,33 @@ const getComputerChoice = () => {
         return "SCISSORS";
     }
 }
+let winCount = 0;
+let loseCount = 0;
 
 const playRound = (playerSelection, computerSelection) => {
     if (playerSelection === computerSelection) {
         return "It is a tie!";
     } else if (playerSelection === "ROCK") {
         if (computerSelection === "SCISSORS") {
+            winCount++
             return "You win! Rock beats Scissors.";
         } else {
+            loseCount++
             return "You lose! Paper beats Rock."}
     } else if (playerSelection === "SCISSORS") {
         if (computerSelection === "PAPER") {
+            winCount++
             return "You win! Scissors beats Paper.";
         } else {
+            loseCount++
             return "You lose! Rock beats Scissors.";
         }
     } else if (playerSelection === "PAPER") {
         if (computerSelection === "ROCK") {
+            winCount++
             return "You win! Paper beats Rock.";
         } else {
+            loseCount++
             return "You lose! Scissors beats Paper.";
         }
     }
